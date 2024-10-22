4831abcebe410550da8ea15514b22092ae7162d8
Francisco Cucullu <francisco.cucullu@gmail.com>
2024-10-22 23:56:06 +0200

    initial components and routing

--- music_controller/frontend/webpack.config.js
+++ music_controller/frontend/webpack.config.js
@@ -1,41 +1,32 @@
 const path = require("path");
 const webpack = require("webpack");
 
-module.exports = (env, argv) => {
-    return {
-        entry: "./src/index.js",
-        output: {
-            path: path.resolve(__dirname, "./static/frontend"),
-            filename: "[name].js",
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./static/frontend"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
         },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: "babel-loader",
-                    },
-                },
-            ],
-        },
-        optimization: {
-            minimize: true,
-        },
-        plugins: [
-            new webpack.DefinePlugin({
-                "process.env": {
-                    NODE_ENV: JSON.stringify(argv.mode === 'production' ? 'production' : 'development'),
-                },
-            }),
-        ],
-        devServer: {
-            static: {
-                directory: path.join(__dirname, "./static/frontend"),
-            },
-            compress: true,
-            port: 8000,
-            hot: true,  // Enables Hot Module Replacement
-        },
-    };
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        // This has effect on the react lib size
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || "development"),
+      },
+    }),
+  ],
 };

4ddc555e9a88507892dd9c08ffe2c355d0381859
Francisco Cucullu <francisco.cucullu@gmail.com>
2024-10-25 00:25:41 +0200

    creating the updating component

--- music_controller/frontend/src/components/Room.js
+++ music_controller/frontend/src/components/Room.js
@@ -1,5 +1,7 @@
+
 import React, { Component } from "react";
 import { Grid, Button, Typography } from "@material-ui/core";
+import CreateRoomPage from "./CreateRoomPage";
 
 export default class Room extends Component {
   constructor(props) {
@@ -8,39 +10,32 @@ export default class Room extends Component {
       votesToSkip: 2,
       guestCanPause: false,
       isHost: false,
+      showSettings: false,
     };
     this.roomCode = this.props.match.params.roomCode;
     this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
-  }
-
-  componentDidMount() {
+    this.updateShowSettings = this.updateShowSettings.bind(this);
+    this.renderSettingsButton = this.renderSettingsButton.bind(this);
+    this.renderSettings = this.renderSettings.bind(this);
+    this.getRoomDetails = this.getRoomDetails.bind(this);
     this.getRoomDetails();
   }
 
   getRoomDetails() {
-    fetch("/api/get-room" + "?code=" + this.roomCode)
+    return fetch("/api/get-room" + "?code=" + this.roomCode)
       .then((response) => {
         if (!response.ok) {
           this.props.leaveRoomCallback();
           this.props.history.push("/");
-          return; // Exit the function if the response is not okay
         }
         return response.json();
       })
       .then((data) => {
-        if (data) {
-          // Ensure data is valid before using it
-          this.setState({
-            votesToSkip: data.votes_to_skip,
-            guestCanPause: data.guest_can_pause,
-            isHost: data.is_host,
-          });
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching room details:", error);
-        this.props.leaveRoomCallback();
-        this.props.history.push("/"); // Redirect on error as well
+        this.setState({
+          votesToSkip: data.votes_to_skip,
+          guestCanPause: data.guest_can_pause,
+          isHost: data.is_host,
+        });
       });
   }
 
@@ -55,30 +50,79 @@ export default class Room extends Component {
     });
   }
 
-  render() {
+  updateShowSettings(value) {
+    this.setState({
+      showSettings: value,
+    });
+  }
+
+  renderSettings() {
     return (
-      <Grid container spacing={1} align="center">
-        <Grid item xs={12}>
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <CreateRoomPage
+            update={true}
+            votesToSkip={this.state.votesToSkip}
+            guestCanPause={this.state.guestCanPause}
+            roomCode={this.roomCode}
+            updateCallback={this.getRoomDetails}
+          />
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => this.updateShowSettings(false)}
+          >
+            Close
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  renderSettingsButton() {
+    return (
+      <Grid item xs={12} align="center">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => this.updateShowSettings(true)}
+        >
+          Settings
+        </Button>
+      </Grid>
+    );
+  }
+
+  render() {
+    if (this.state.showSettings) {
+      return this.renderSettings();
+    }
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
           <Typography variant="h4" component="h4">
             Code: {this.roomCode}
           </Typography>
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={12} align="center">
           <Typography variant="h6" component="h6">
             Votes: {this.state.votesToSkip}
           </Typography>
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={12} align="center">
           <Typography variant="h6" component="h6">
-            Guest can pause: {this.state.guestCanPause.toString()}
+            Guest Can Pause: {this.state.guestCanPause.toString()}
           </Typography>
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={12} align="center">
           <Typography variant="h6" component="h6">
             Host: {this.state.isHost.toString()}
           </Typography>
         </Grid>
-        <Grid item xs={12}>
+        {this.state.isHost ? this.renderSettingsButton() : null}
+        <Grid item xs={12} align="center">
           <Button
             variant="contained"
             color="secondary"
